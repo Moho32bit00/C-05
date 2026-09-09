d33746bb671d6e2225ab36724e1127697ab145e1
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-09 17:46:41 +0300

    Q1 answer

--- C05_C#/Program.cs
+++ C05_C#/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace C05_C_
 {
+    #region q1 
+    //public class Book
+    //{
+    //    private string password = "secret";
+    //};
+    #endregion 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1 
+            //Book b1 = new Book();
+            //Console.WriteLine(b1.password); 
+            ////it is not going to print , cuz this is a private field so it can not be used outside the class .
+            #endregion 
         }
     }
 }
